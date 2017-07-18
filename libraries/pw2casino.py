8ce0c78a551dd7737642be55eade02544e0e8a01
kayahans <saritaskayahan@gmail.com>
2017-07-18 13:54:03 -0400

    pw2casino fixes

--- libraries/pw2casino.py
+++ libraries/pw2casino.py
@@ -84,8 +84,7 @@ class pw2casino:
                         header+=str(self.dft.system.scell_size)+'\n'
                     elif line == kpoint_s:
                         i += 1
-                        sys.stdout.write(str(float(i)/float(numkpts)))
-                        sys.stdout.flush()
+                        print str(float(i) / float(numkpts)) + "\r"
                         first=False
                     else:
                         header += line
@@ -93,8 +92,8 @@ class pw2casino:
                     if line == kpoint_s:
                         new=True
                         i += 1
-                        sys.stdout.write(str(float(i) / float(numkpts)))
-                        sys.stdout.flush()
+                        print str(float(i) / float(numkpts))+"\r"
+
                         k_info = line
                         k_list.append(
                             kpoints(num=k_info[0], nbnds_up=k_info[1], nbnds_down=k_info[2], coords=k_info[3:]))
